1b29497a1e9a8efc83324dc7a1628055e6560d88
William Alves <williamlvs0072@outlook.com>
2024-06-03 11:20:21 -0400

    adicionada a conexão com o backend

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,27 @@
-function getUrlEncurtada() {
-    var urlEncurtada = "urlencurtada.com";
-    document.getElementById("urlContainer").innerHTML =
-    `<p>${urlEncurtada}</p>`;
-}
+document.getElementById("urlForm").addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const urlInput = document.getElementById("urlInput").value;
+
+    try {
+      const response = await fetch("http://localhost:8080/api/v1/processBitly", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ url: urlInput }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        document.getElementById("resultado").innerText = `${data.shortUrl}`;
+      } else {
+        document.getElementById(
+          "resultado"
+        ).innerText = `Erro: ${data.message}`;
+      }
+    } catch (error) {
+      console.error("Erro ao enviar solicitação:", error);
+    }
+  });
